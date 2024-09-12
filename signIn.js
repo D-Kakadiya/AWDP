9926b3e234f728a861eb985884e22d8110794405
D-Kakadiya <154602557+D-Kakadiya@users.noreply.github.com>
2024-09-12 22:13:25 +0530

    Add files via upload

---
+++ signIn.js
@@ -0,0 +1,53 @@
+let name = localStorage.getItem("first_name") ? localStorage.getItem("first_name") : ""
+let user_phoneno = localStorage.getItem("phone_number") ? localStorage.getItem("phone_number") : ""
+let user_address = localStorage.getItem("address") ? localStorage.getItem("address") : ""
+
+function validateForm(){
+
+    var email = document.myform.email.value;
+    var password = document.myform.password.value;
+
+    if (email === null || email === "") {
+        document.getElementById("e1").innerHTML = "Email is Required*"
+    }
+    if (password === null || password === "") {
+        document.getElementById("e2").innerHTML = "Password is Requied*"
+    }
+    else {
+        saveData()
+    }
+}
+
+function saveData(){
+
+    let email,password;
+
+    email = document.getElementById("email").value;
+    password = document.getElementById("password").value;
+
+    let user_records = [];
+    user_records = JSON.parse(localStorage.getItem("users")) ? JSON.parse(localStorage.getItem("users")) : []
+    
+    if (user_records.some((v) => {return v.email == email && v.password == password})) {
+
+        let current_user = user_records.find((v) =>  v.email === email)
+
+        let loggedInUser = {
+            name: current_user?.first_name,
+            email : current_user?.email,
+            user_phoneno : current_user?.phone_number,
+            user_address : current_user?.address
+        }
+        
+        localStorage.setItem("loggedInUser", JSON.stringify(loggedInUser));
+
+        window.location.href = "/profile.html";
+       
+        alert("Welcome");
+
+    } else{
+        alert("Password for" + email + "is wrong, Please try Again");
+    }
+}
+
+
---
+++ signUp.js
@@ -0,0 +1,83 @@
+function saveData() {
+    let first_name, last_name, email, password, phone_number, address;
+
+    first_name = document.getElementById("first_name").value;
+    last_name = document.getElementById("last_name").value;
+    email = document.getElementById("email").value;
+    password = document.getElementById("password").value;
+    phone_number = document.getElementById("phone_number").value;
+    address = document.getElementById("address").value;
+
+    let user_records = [];
+    user_records = JSON.parse(localStorage.getItem("users")) || []
+
+    if (user_records.some((v) => { return v.email == email })) {
+        alert(email + "is already register.")
+    }
+    else {
+        user_records.push({
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": email,
+            "password": password,
+            "phone_number": phone_number,
+            "address": address
+        })
+
+        localStorage.setItem("users", JSON.stringify(user_records))
+        window.location.href = "/signIn.html"
+        alert(email + " Thanks for Registration. \n Try to Login Now")
+    }
+
+
+}
+
+function validateForm() {
+
+    var first_name = document.myForm.first_name.value;
+    var last_name = document.myForm.last_name.value;
+    var email = document.myForm.email.value;
+    var password = document.myForm.password.value;
+    var phone_number = document.myForm.phone_number.value;
+    var date = document.myForm.date.value;
+    var address = document.myForm.address.value;
+    var gender = document.myForm.gender.value;
+
+    if (first_name === "" && last_name === "" && email === "" && password === "" && phone_number === "" && address === "" && gender == "") {
+        if (first_name == null || first_name == "") {
+            document.getElementById("e1").innerHTML = "First Name is Required*";
+        }
+        if (last_name == null || last_name == "") {
+            document.getElementById("e2").innerHTML = "Last Name is Required*";
+        }
+        if (email == null || email == "") {
+            document.getElementById("e3").innerHTML = "Email is Required*";
+        }
+        if (password == null || password == "") {
+            document.getElementById("e4").innerHTML = "Password is Required*";
+        }
+        if (phone_number == null || phone_number == "") {
+            document.getElementById("e5").innerHTML = "Phone Number is Required*";
+        }
+        if (date == null || date == "") {
+            document.getElementById("e6").innerHTML = "Birth Date is Required*";
+        }
+        if (address == null || address == "") {
+            document.getElementById("e7").innerHTML = "Address is Required*";
+        }
+        if (password.length < 6) {
+            document.getElementById("e4").innerHTML = "Password length must be atleast 8 characters*";
+        }
+        if (password.length > 15) {
+            document.getElementById("e4").innerHTML = "Password length must not exceed 15 characters*";
+        }
+        if (gender == null || gender == "") {
+            document.getElementById("e8").innerHTML = "Required*";
+        }
+        return false;
+    } else {
+        saveData()
+    }
+
+}
+
